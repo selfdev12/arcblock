7dbc35fcd1c833fbe78503ea1e062391fb708734
jing <jingyuan.ai@spacelabs.com>
2018-10-24 11:59:59 -0700

    add extract

--- GetTopnFromUrl.py
+++ GetTopnFromUrl.py
@@ -9,24 +9,30 @@ def fetch_content(url):
             lines.append(line_word)
     return lines
 
-def select_topn(lines, N):
-    result = {}
+def extract(lines):
+    lst = []
     for line in lines:
-        #ignore the first line since it is a title
-        if(line.startswith('name')):
+        # ignore the first line since it is a title
+        if (line.startswith('name')):
             continue
         words = line.split(",")
+        lst.append(words)
+    return lst
+
+def select_topn(lst, sortcolumn, N):
+    result = {}
+    for line in lst:
         if((not result) or (len(result) < N)):
-            result[words[2]] = line
+            result[line[sortcolumn]] = line
         #if already has N line, replace the lowest line if current one is bigger in rating
         elif(len(result) == N):
             lowest = list(result.keys())[0]
             for k in result.keys():
                 if(lowest > k):
                     lowest = k
-            if(lowest < words[2]):
+            if(lowest < line[sortcolumn]):
                 del result[lowest]
-                result[words[2]] = line
+                result[line[sortcolumn]] = line
 
     return result.values()
 
@@ -38,7 +44,8 @@ def print_topn(topn_lines):
 def main(arg1, arg2):
     #https://gist.githubusercontent.com/tyrchen/32c50aadca48aee3da10a77a18479517/raw/3aa07629e61239cd26cf514584c949a98aa38d67/movies.csv
     lines = fetch_content(arg1)
-    topNitems = select_topn(lines, int(arg2))
+    lst = extract(lines)
+    topNitems = select_topn(lines, 2, int(arg2))
     print_topn(topNitems)
 
 
